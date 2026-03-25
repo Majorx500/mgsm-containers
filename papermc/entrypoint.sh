4ec9f0b409e2e250ecd86a045dab0ebc8e592b67
Majorx500 <majorx500@gmail.com>
2026-03-26 00:20:27 +0100

    working papermc container running

--- papermc/entrypoint.sh
+++ papermc/entrypoint.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 # $1 -- gameversion $2 -- gamedir
-
 base_dir=./
-
-echo "$JAVA_ARGS"
+JAVA_ARGS="java"
 if [[ -e "$MEMORY_SIZE" ]]; then
+
   JAVA_ARGS="$JAVA_ARGS -Xms$MEMORY_SIZE -Xmx$MEMORY_SIZE"
 fi
-if [[ -d $base_dir/serverfiles ]]; then
-  cd serverfiles || exit
-  java "$JAVA_ARGS" -jar papermc.jar -nogui
+
+if [[ ! -d $base_dir/serverfiles ]]; then
   game_url=$(curl -X 'GET' \
     "https://fill.papermc.io/v3/projects/paper/versions/$GAME_VERSION/builds/latest" \
     -H 'accept: application/json' | jq -r '.downloads."server:default".url')
-  mkdir -p ./serverfiles
 
-  wget "$game_url" -O "./serverfiles/papermc.jar" 1>/dev/null
-  chmod +x ".//serverfiles/papermc.jar"
+  wget "$game_url" -O "./papermc.jar" 1>/dev/null
+  chmod +x "./papermc.jar"
 
-  echo "eula=true" >./serverfiles/eula.txt
-
+  echo "eula=true" >./eula.txt
   echo "Installing server"
-  cd serverfiles || exit
-  java -jar "$JAVA_ARGS" -nogui papermc.jar
+
+  echo "stop" | "$JAVA_ARGS" -jar papermc.jar -nogui
 fi
+
+if [[ ! $(id -u) -eq $UID ]]; then
+  JAVA_ARGS="gosu mgsm:mgsm $JAVA_ARGS"
+  chown -vR $UID:$GID /data
+fi
+
+exec $JAVA_ARGS -jar papermc.jar -nogui
