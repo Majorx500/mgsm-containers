edb0c659f74b99c437f6afe90608ca62d9ca7297
Majorx500 <majorx500@gmail.com>
2026-04-03 12:09:16 +0200

    fixed entrypoint.sh

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 if [[ -f "steamcmd.sh" ]]; then
-  echo "SteamCMD found, exiting"
-  ./steamcmd.sh
+  echo "SteamCMD found"
+  ./steamcmd.sh +exit
   exit 0
 fi
-
-echo "Installing Steamcmd"
-
-curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
-
 chown mgsm:mgsm -R ./*
-
 ./steamcmd.sh +exit
